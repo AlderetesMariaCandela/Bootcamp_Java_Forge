7287abfc638dba7c41b28189938e440e61bf9da6
Maria Alderetes <alderetesvmaria@gmail.com>
2023-07-28 15:04:04 -0300

    Pitagoras

---
+++ TeoremaPitagoras/PitagorasTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest {
+    public static void main (String[] args){
+        Pitagoras hipotenusa = new Pitagoras();
+        double result = hipotenusa.calcularHipotenusa(3,5);
+        System.out.println("La hipotenusa es "+ result);
+    }
+}
