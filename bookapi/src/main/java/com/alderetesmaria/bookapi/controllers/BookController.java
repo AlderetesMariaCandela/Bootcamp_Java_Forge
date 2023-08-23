644f36741e401c15d874050f2521dc42ff1e6792
Maria Alderetes <alderetesvmaria@gmail.com>
2023-08-23 19:25:25 -0300

    Actualizar y Eliminar

---
+++ bookapi/src/main/java/com/alderetesmaria/bookapi/controllers/BookController.java
@@ -0,0 +1,5 @@
+package com.alderetesmaria.bookapi.controllers;
+
+public class BookController {
+
+}
---
+++ bookapi/src/test/java/com/alderetesmaria/bookapi/BookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alderetesmaria.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
